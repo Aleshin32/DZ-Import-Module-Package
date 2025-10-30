bde5d699c674f6be861d8d40b2b1029e2230fb5f
Алешин Алексей <103904231+Aleshin32@users.noreply.github.com>
2025-10-30 23:36:15 +0300

    Add files via upload

---
+++ Accounting/main.py
@@ -0,0 +1,8 @@
+import application.db.people
+from application.salary import calculate_salary
+
+if __name__ == '__main__':
+    print(datetime.now())
+    application.db.people.get_employees()
+    calculate_salary()
+
